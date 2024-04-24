34a1e5ac82a09b7b1331fa5577af9cd6bfca7167
Yauheni Kirychenka <yauhenikirychenka@gmail.com>
2024-04-24 09:00:57 +0300

    Feat: add css variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(dirname, "src/styles")],
+    prependData: `@import "variables.scss";`,
+  },
+};
 
 export default nextConfig;
