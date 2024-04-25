584d1e7dc06a7cf3146f8f2596170307b1f09ab6
Yauheni Kirychenka <yauhenikirychenka@gmail.com>
2024-04-25 15:40:32 +0300

    Feat: add home page intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import styles from "./page.module.scss";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <header>
+        <h1 className="visually-hidden">
+          Languanarium - useful platform for language learners
+        </h1>
+        <p className={styles.lead}>
+          Find friends that learn your language.
+          <br />
+          Use useful tools.
+          <br />
+          Lets study together!
+        </p>
+      </header>
+    </main>
+  );
 }
