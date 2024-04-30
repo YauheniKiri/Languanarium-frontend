cff65b51e3d41f211e2dd9bf1f3ea02f5e3441a9
Yauheni Kirychenka <yauhenikirychenka@gmail.com>
2024-04-30 16:24:32 +0300

    Feat: replace normalize.css to mui's reset component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/src/styles/global.scss";
 import styles from "./page.module.scss";
 import type { Metadata } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import CssBaseline from "@mui/material/CssBaseline";
 import Navigation from "@/src/ui/Navigation/Navigation";
 import { platypi } from "@/fonts";
 import Wrapper from "../ui/Wrapper/Wrapper";
@@ -20,13 +21,15 @@ export default function RootLayout({
     <html className={platypi.className} lang="en">
       <body className={styles.container}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <header className={styles.header}>
-            <Wrapper>
-              <Navigation></Navigation>
-            </Wrapper>
-          </header>
-          {children}
-          <footer className={styles.footer}></footer>
+          <CssBaseline>
+            <header className={styles.header}>
+              <Wrapper>
+                <Navigation></Navigation>
+              </Wrapper>
+            </header>
+            {children}
+            <footer className={styles.footer}></footer>
+          </CssBaseline>
         </AppRouterCacheProvider>
       </body>
     </html>
