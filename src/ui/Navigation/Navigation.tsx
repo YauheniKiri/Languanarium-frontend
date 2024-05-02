b708f5041c14da6056630222361b50852819c1db
Yauheni Kirychenka <yauhenikirychenka@gmail.com>
2024-05-02 17:14:43 +0300

    Feat: add navigation for mobile devices

--- src/ui/Navigation/Navigation.tsx
+++ src/ui/Navigation/Navigation.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import styles from "./navigation.module.scss";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+import CloseIcon from "@mui/icons-material/Close";
 
 const navigation = [
   {
@@ -17,26 +20,52 @@ const navigation = [
 ];
 
 export default function Navigation() {
+  const [isOpened, setIsOpened] = useState(true);
   const path = usePathname();
 
-  return (
-    <nav>
-      <ul className={styles.menu}>
-        {navigation.map(({ name, href }, index) => {
-          const isActive = path === href;
+  const handleMenuButtonClick = () => {
+    setIsOpened((isOpened) => !isOpened);
+  };
 
-          return (
-            <li key={index}>
-              <Link
-                className={clsx(styles.item, isActive && styles.active)}
-                href={href}
-              >
-                {name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+  const menuButtonIcon = isOpened ? (
+    <CloseIcon className={styles.icon} />
+  ) : (
+    <FormatAlignLeftIcon className={styles.icon} />
+  );
+
+  return (
+    <div
+      className={clsx(styles.container, isOpened && styles["mobile-opened"])}
+    >
+      <div className={styles["button-wrapper"]}>
+        <button
+          onClick={handleMenuButtonClick}
+          className={styles.button}
+          aria-label={`${isOpened ? "Close" : "Open"} navigation`}
+        >
+          {menuButtonIcon}
+        </button>
+      </div>
+      {isOpened && (
+        <nav>
+          <ul className={styles.menu}>
+            {navigation.map(({ name, href }, index) => {
+              const isActive = path === href;
+
+              return (
+                <li key={index}>
+                  <Link
+                    className={clsx(styles.item, isActive && styles.active)}
+                    href={href}
+                  >
+                    {name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      )}
+    </div>
   );
 }
